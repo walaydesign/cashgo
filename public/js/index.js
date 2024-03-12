b52d4081f69ae4770a52d1af5a6f7da501cfa33d
youzhen <yl301114@gmail.com>
2024-03-12 15:15:44 +0000

    menu, fade in

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+AOS.init({startEvent: 'load'});
+window.addEventListener('load', AOS.refresh);
+
 $(".header_menu").click(function(){
     $(this).toggleClass("active");
     $(".header_list").toggleClass("active");
@@ -22,5 +25,7 @@ $(".gotoelement").click(function(){
     let target = $(this).data("target");
     let top = $(target).offset().top - 60;
     $("html, body").animate({scrollTop:top},300);
+    $(".header_list").removeClass("active");
+    $(".header_menu").removeClass("active");
 })
 
