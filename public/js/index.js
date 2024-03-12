b00aaba1efb9fa7046f44f6afa4bccb710499969
youzhen <yl301114@gmail.com>
2024-03-12 14:33:28 +0000

    finish

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+$(".header_menu").click(function(){
+    $(this).toggleClass("active");
+    $(".header_list").toggleClass("active");
+})
+
+$(".qa_q").click(function(){
+    $(this).toggleClass("active");
+    $(this).parents(".qa_item").find(".qa_a").slideToggle(300);
+})
+
+$(".qa_search_btn").click(function(){
+    if(!$(this).parents(".qa_search-wrap").hasClass("active")) {
+        $(this).parents(".qa_search-wrap").addClass("active");
+    }
+})
+
+$(".qa_search_close").click(function(){
+    $(this).parents(".qa_search-wrap").removeClass("active");
+})
+
+$(".gotoelement").click(function(){
+    let target = $(this).data("target");
+    let top = $(target).offset().top - 60;
+    $("html, body").animate({scrollTop:top},300);
+})
+
